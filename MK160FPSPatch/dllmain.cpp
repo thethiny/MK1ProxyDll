e85b6231001ded12a2a93b4b4f4a8f3b9b02ebb3
thethiny <thethiny@ymail.com>
2023-10-07 04:11:00 +0400

    1.0.1: Add GetLastError with MessageBox

--- MK160FPSPatch/dllmain.cpp
+++ MK160FPSPatch/dllmain.cpp
@@ -8,31 +8,53 @@ HANDLE hConsole = NULL;
 
 void LoadECPFiles()
 {
-	HMODULE hModule = GetModuleHandle(nullptr);
-	WCHAR path[MAX_PATH];
-	GetModuleFileName(hModule, path, MAX_PATH);
+    HMODULE hModule = GetModuleHandle(nullptr);
+    WCHAR path[MAX_PATH];
+    GetModuleFileName(hModule, path, MAX_PATH);
 
-	std::filesystem::path filePath(path);
-	std::filesystem::path directoryPath = filePath.parent_path();
+    std::filesystem::path filePath(path);
+    std::filesystem::path directoryPath = filePath.parent_path();
 
-	for (const auto& entry : std::filesystem::directory_iterator(directoryPath))
-	{
-		if (wcscmp(L".ehp", entry.path().extension().c_str()) == 0)
-		{
-			wprintf(L"Loading Hook Plugin %s!\n", entry.path().c_str());
-			auto libName = entry.path().c_str();
-			HMODULE module = LoadLibrary(libName);
-			if (module)
-			{
-				printf("Plugin Loaded Successfully!\n");
-			} 
-			else
-			{
-				printf("Failed to load Plugin!\n");
-			}
-		}
-	}
+    for (const auto& entry : std::filesystem::directory_iterator(directoryPath))
+    {
+        if (wcscmp(L".ehp", entry.path().extension().c_str()) == 0)
+        {
+            wprintf(L"Loading Hook Plugin %s!\n", entry.path().c_str());
+            auto libName = entry.path().c_str();
+            HMODULE module = LoadLibrary(libName);
+            if (module)
+            {
+                printf("Plugin Loaded Successfully!\n");
+            }
+            else
+            {
+                DWORD error = GetLastError();
+                LPVOID errorMessage;
+                FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+                    NULL,
+                    error,
+                    0, // Default language
+                    (LPWSTR)&errorMessage,
+                    0,
+                    NULL);
 
+                // Print error message
+                wprintf(L"Failed to load plugin (Error %d): %s\n", error, errorMessage);
+
+                // Create the message text with "Failed to load plugin" and the error message
+                WCHAR message[512];
+                swprintf_s(message, 512, L"Failed to load plugin: %s", errorMessage);
+
+                // Create the title text with "Error: %d" where %d is the error code
+                WCHAR title[128];
+                swprintf_s(title, 128, L"Error: %d", error);
+
+                MessageBox(NULL, message, title, MB_ICONERROR);
+
+                LocalFree(errorMessage); // Free the error message buffer
+            }
+        }
+    }
 }
 
 void CreateConsole()
